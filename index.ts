b52344ef4fd28acbecbe7e525886969d30ecec54
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-10-17 23:51:16 +0900

    Simple diff of JS and TS

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello world")
+const name = "Jaeeun",
+age = 24,
+gender = "female";
+
+const sayHi = (name, age, gender?) => {
+    console.log('Hello ${name}, you are ${age}, you are a ${gender}');
+};
+
+sayHi(name, age); 
+// sayHi parameter의 gender 끝에 ? 없으면 에러 발생
+//JavaScript는 그렇지 않음
+
+export {};
