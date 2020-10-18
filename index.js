fc3c0da2cdcf66a34caee5c0376014b675246756
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-10-19 00:03:39 +0900

    Add settings to automate TS -> JS

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(2, "24", 14);
-//# sourceMappingURL=index.js.map
