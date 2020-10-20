6a3187e300f45ad7e607c6a9cf82ec5b95afadee
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-10-20 23:29:18 +0900

    Add interface of TS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-const sayHi = (name:string, age:Number, gender:string): void => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "jaeeun",
+    age: 22,
+    gender: "female",
+}
+
+const sayHi = (person: Human): void => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
     // return 사용 불가 (:void)
 };
 // 타입 설정함
 
-sayHi("jaeeun", 24, "female")
+sayHi(person)
 //sayHi("2", "24", 14); 
 // 여러 개가 틀려도 첫번째 거만 찾아줌
 
