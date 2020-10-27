563b8aeacc933b08823a3a995dd32993f03586de
jeje01 <jje141@gmail.com>
2020-10-27 23:57:21 +0900

    Add method to add block if valid

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,14 @@ const createNewBlock = (data:string): Block => {
     return newBlock;
 };
 
+const getHashforBlock = (aBlock: Block): string =>
+    Block.calculateBlockHash(
+        aBlock.index,
+        aBlock.previousHash,
+        aBlock.timestamp,
+        aBlock.data
+    );
+
 const isBlockValid = (
     candidateBlock : Block, 
     previousBlock : Block
@@ -77,6 +85,16 @@ const isBlockValid = (
         return false;
     } else if(previousBlock.hash !== candidateBlock.previousHash){
         return false;
+    } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+    if(isBlockValid(candidateBlock, getLatestBlock())){
+        blockchain.push(candidateBlock);
     }
 };
 
